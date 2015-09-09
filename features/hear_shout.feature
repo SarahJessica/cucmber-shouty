79729bc49be4dcdbd1039ccbb357508399548778
Sarah Jessica Leivers <sarahjessica.leivers@bbc.co.uk>
2015-09-09 16:08:04 +0100

    day's training no refactoring

--- features/hear_shout.feature
+++ features/hear_shout.feature
@@ -1,3 +1,25 @@
-Feature: Hear Shout
+Feature: Simple Shout
 
-  A description goes here...
+  Rules:
+  - Messages are only received within a radius of 1000m
+  - Messages must be between 1 and 140 characters UTF-8
+
+  Questions:
+  - Do we call it a message or a shout?
+  - Will I receive my neighbour's shout from this morning when I come home?
+  - Do we need to worry about profanity?
+  - What does the UI look like?
+
+  Persona:
+  - Joanne is a listener
+  - Fred is a shouter
+
+  Scenario: Joanne is too far away to hear Fred
+    Given Joanne is 2 km away from Fred
+    When Fred shouts
+    Then Joanne does not hear anything
+
+  Scenario: Joanne is close enough to hear Fred
+    Given Joanne is 1 km away from Fred
+    When Fred shouts
+    Then Joanne hears Fred's shout
--- features/step_definitions/shout_steps.js
+++ features/step_definitions/shout_steps.js
@@ -1,9 +1,89 @@
+
 var assert = require('assert');
 var Shouty = require('../../lib/shouty');
+
+var bridgeHouse = [53.4720597,-2.2998615];
+var theQuays    = [53.4709477,-2.2938976];
+var piccadilly  = [53.4774029,-2.2309325];
 
 module.exports = function () {
   this.World = function(callback) {
     callback();
     return new Shouty();
   };
+
+  this.Given(/^Joanne is (\d+) km away from Fred$/, function (distance, callback) {
+    this.personIsIn("Joanne", bridgeHouse);
+
+    if(distance <= 1) {
+      this.personIsIn("Fred", theQuays);
+    } else {
+      this.personIsIn("Fred", piccadilly);
+    }
+    callback();
+  });
+
+  this.When(/^Fred shouts$/, function (callback) {
+    this.shout("Fred", "hello everyone");
+    // Write code here that turns the phrase above into concrete actions
+    callback();
+  });
+
+  this.Then(/^Joanne does not hear anything$/, function (callback) {
+    var expected = [];
+    var actual = this.getAllShoutsHeardBy("Joanne");
+    assert.deepEqual(actual, expected);
+    callback();
+  });
+
+  this.Then(/^Joanne hears Fred's shout$/, function (callback) {
+    var expected = ["hello everyone"];
+    var actual = this.getAllShoutsHeardBy("Joanne");
+    assert.deepEqual(actual, expected);
+    callback();
+  });
+};
+var assert = require('assert');
+var Shouty = require('../../lib/shouty');
+
+var bridgeHouse = [53.4720597,-2.2998615];
+var theQuays    = [53.4709477,-2.2938976];
+var piccadilly  = [53.4774029,-2.2309325];
+
+module.exports = function () {
+  this.World = function(callback) {
+    callback();
+    return new Shouty();
+  };
+
+  this.Given(/^Joanne is (\d+) km away from Fred$/, function (distance, callback) {
+    this.personIsIn("Joanne", bridgeHouse);
+
+    if(distance <= 1) {
+      this.personIsIn("Fred", theQuays);
+    } else {
+      this.personIsIn("Fred", piccadilly);
+    }
+    callback();
+  });
+
+  this.When(/^Fred shouts$/, function (callback) {
+    this.shout("Fred", "hello everyone");
+    // Write code here that turns the phrase above into concrete actions
+    callback();
+  });
+
+  this.Then(/^Joanne does not hear anything$/, function (callback) {
+    var expected = [];
+    var actual = this.getAllShoutsHeardBy("Joanne");
+    assert.deepEqual(actual, expected);
+    callback();
+  });
+
+  this.Then(/^Joanne hears Fred's shout$/, function (callback) {
+    var expected = ["hello everyone"];
+    var actual = this.getAllShoutsHeardBy("Joanne");
+    assert.deepEqual(actual, expected);
+    callback();
+  });
 };
